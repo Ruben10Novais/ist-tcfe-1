2d1096b177ed9a74b33a626d4ba94923ebc5dce6
Ruben10Novais <80060211+Ruben10Novais@users.noreply.github.com>
2021-03-18 13:28:58 +0000

    ANOTHER ONE

--- t1/mat/mesh_node.m
+++ t1/mat/mesh_node.m
@@ -6,34 +6,31 @@ clear all
 pkg load symbolic
 
 %% MESH AND NODE ANALYSIS 
-Z = vpa(0.0)
-O = vpa(1.0)
-
-R1 = vpa(1021.83054686)
-R2 = vpa(2005.19295062)
-R3 = vpa(3017.35849578)
-R4 = vpa(4151.36004156)
-R5 = vpa(3044.17402199)
-R6 = vpa(2021.96984898)
-R7 = vpa(1007.84334281)
-G1 = O/R1
-G2 = O/R2
-G3 = O/R3
-G4 = O/R4
-G5 = O/R5
-G6 = O/R6
-G7 = O/R7
-V = vpa(5.11339870586)
-I = vpa(0.0010050415923)
-Kb = vpa(0.00718106630963)
-Kc = vpa(8122.7111635)
+R1 = 1.02183054686e3 %% Ohm
+R2 = 2.00519295062e3
+R3 = 3.01735849578e3
+R4 = 4.15136004156e3
+R5 = 3.04417402199e3
+R6 = 2.02196984898e3
+R7 = 1.00784334281e3
+G1 = 1/R1
+G2 = 1/R2
+G3 = 1/R3
+G4 = 1/R4
+G5 = 1/R5
+G6 = 1/R6
+G7 = 1/R7
+V = 5.11339870586 %% V
+I = 1.0050415923e-3 %% A
+Kb = 7.18106630963e-3 %% S
+Kc = 8.1227111635e3 %% Ohm
 
 %% Matriz-resultado com valores numéricos das correntes nas malhas
-A = [Z, Z, Z, O; R1+R3+R4, -R3, -R4, Z; -R4, Z, R6+R7+R4-Kc, Z; -R3*Kb, R3*Kb - O, Z, Z]
-B = [I; -V; Z; Z]
+A = [0, 0, 0, 1; R1+R3+R4, -R3, -R4, 0; -R4, 0, R6+R7+R4-Kc, 0; -R3*Kb, R3*Kb - 1, 0, 0]
+B = [I; -V; 0; 0]
 A\B
 
 %% Matriz-resultado com valores numéricos da tensão nos nós do circuito (NODE)
-C = [G7, Z, Z, Z, Z, Z, G6, Z; O, -O, Z, Z, Z, Z, Z, Z; Z, Z, Z, Z, Z, O, -O, Z; Z, Z, Z, -G2, G1+G2+G3, -G1, Z, -G3; Z, Z, G5, Z, G3, Z, G4+G6, -G3-G4-G5; O, Z, Z, Z, Z, Z, Kc*G6, -O; Z, Z, Z, Z, G1, -G1, -G4-G6, G4; Z, Z, Z, -G2, Kb+G2, Z, Z, -Kb]
-D = [Z; Z; V; Z; I; Z; Z; Z]
+C = [G7, 0, 0, 0, 0, 0, G6, 0; 1, -1, 0, 0, 0, 0, 0, 0; 0, 0, 0, 0, 0, 1, -1, 0; 0, 0, 0, -G2, G1+G2+G3, -G1, 0, -G3; 0, 0, G5, 0, G3, 0, G4+G6, -G3-G4-G5; 1, 0, 0, 0, 0, 0, Kc*G6, -1; 0, 0, 0, 0, G1, -G1, -G4-G6, G4; 0, 0, 0, -G2, Kb+G2, 0, 0, -Kb]
+D = [0; 0; V; 0; I; 0; 0; 0]
 C\D
