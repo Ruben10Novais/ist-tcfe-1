6d374fd0645688cac27d17f56521b6cf0e44d6e6
Ruben10Novais <ruben.novais2001@gmail.com>
2021-03-19 02:29:34 +0000

    FINNALY... JEEZ

--- t1/mat/mesh_node.m
+++ t1/mat/mesh_node.m
@@ -28,9 +28,43 @@ Kc = 8.1227111635e3 %% Ohm
 %% Matriz-resultado com valores numéricos das correntes nas malhas
 A = [0, 0, 0, 1; R1+R3+R4, -R3, -R4, 0; -R4, 0, R6+R7+R4-Kc, 0; -R3*Kb, R3*Kb - 1, 0, 0]
 B = [I; -V; 0; 0]
-A\B
+C = A\B
 
 %% Matriz-resultado com valores numéricos da tensão nos nós do circuito (NODE)
-C = [G7, 0, 0, 0, 0, 0, G6, 0; 1, -1, 0, 0, 0, 0, 0, 0; 0, 0, 0, 0, 0, 1, -1, 0; 0, 0, 0, -G2, G1+G2+G3, -G1, 0, -G3; 0, 0, G5, 0, G3, 0, G4+G6, -G3-G4-G5; 1, 0, 0, 0, 0, 0, Kc*G6, -1; 0, 0, 0, 0, G1, -G1, -G4-G6, G4; 0, 0, 0, -G2, Kb+G2, 0, 0, -Kb]
-D = [0; 0; V; 0; I; 0; 0; 0]
-C\D
+D = [G7, 0, 0, 0, 0, 0, G6, 0; 1, -1, 0, 0, 0, 0, 0, 0; 0, 0, 0, 0, 0, 1, -1, 0; 0, 0, 0, -G2, G1+G2+G3, -G1, 0, -G3; 0, 0, G5, 0, G3, 0, G4+G6, -G3-G4-G5; 1, 0, 0, 0, 0, 0, Kc*G6, -1; 0, 0, 0, 0, G1, -G1, -G4-G6, G4; 0, 0, 0, -G2, Kb+G2, 0, 0, -Kb]
+E = [0; 0; V; 0; I; 0; 0; 0]
+F = D\E
+
+%% Atribuir às variáveis os seus valores
+Ia = C(1,1)
+Ib = C(2,1)
+Ic = C(3,1)
+Id = C(4,1)
+
+V1 = F(1,1)
+V2 = F(2,1)
+V3 = F(3,1)
+V4 = F(4,1)
+V5 = F(5,1)
+V6 = F(6,1)
+V7 = F(7,1)
+V8 = F(8,1)
+
+%% Criar 2 ficheiros, que serão o input das tabelas com os resultados teóricos
+fid = fopen("data_current_tab.tex","w")
+fprintf(fid, "@$I_{a}$ & %f \\\\ \\hline \n", Ia)
+fprintf(fid, "@$I_{b}$ & %f \\\\ \\hline \n", Ib)
+fprintf(fid, "@$I_{c}$ & %f \\\\ \\hline \n", Ic)
+fprintf(fid, "@$I_{d}$ & %f \\\\ \\hline \n", Id)
+fclose(fid)
+
+fid = fopen("data_voltage_tab.tex","w")
+fprintf(fid, "$V_{1}$ & %f \\\\ \\hline \n", V1)
+fprintf(fid, "$V_{2}$ & %f \\\\ \\hline \n", V2)
+fprintf(fid, "$V_{3}$ & %f \\\\ \\hline \n", V3)
+fprintf(fid, "$V_{4}$ & %f \\\\ \\hline \n", V4)
+fprintf(fid, "$V_{5}$ & %f \\\\ \\hline \n", V5)
+fprintf(fid, "$V_{6}$ & %f \\\\ \\hline \n", V6)
+fprintf(fid, "$V_{7}$ & %f \\\\ \\hline \n", V7)
+fprintf(fid, "$V_{8}$ & %f \\\\ \\hline \n", V8)
+fclose(fid)
