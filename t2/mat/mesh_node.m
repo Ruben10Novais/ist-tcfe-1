ddf4e491cfb280620e440b32c510b4c04ebafbd4
henrymvb <henry.machado@tecnico.ulisboa.pt>
2021-04-01 17:25:38 +0100

    maionese uwu

--- t2/mat/mesh_node.m
+++ t2/mat/mesh_node.m
@@ -7,71 +7,78 @@ pkg load symbolic
 
 format long
 
-fidd = fopen("data.txt","r")
-cte = fscanf(fidd,'%s = %f', [3 inf])
-printf("valor %f\n", cte(3,1))
-fclose(fidd)
+fp=fopen('data.txt', 'r');
+S=fscanf(fp,'%s');
+R=sscanf(S, 'Pleaseentertheloweststudentnumberinyourgroup:Unitsforthevalues:V,mA,kOhm,mSanduFValues:R1=%fR2=%fR3=%fR4=%fR5=%fR6=%fR7=%fVs=%fC=%fKb=%fKd=%f');
+
+fclose(fp);
 
 %% MESH AND NODE ANALYSIS 
-R1 = 1.02183054686e3 %% Ohm
-R2 = 2.00519295062e3
-R3 = 3.01735849578e3
-R4 = 4.15136004156e3
-R5 = 3.04417402199e3
-R6 = 2.02196984898e3
-R7 = 1.00784334281e3
-G1 = 1/R1
-G2 = 1/R2
-G3 = 1/R3
-G4 = 1/R4
-G5 = 1/R5
-G6 = 1/R6
-G7 = 1/R7
-V = 5.11339870586 %% V
-I = 1.0050415923e-3 %% A
-Kb = 7.18106630963e-3 %% S
-Kc = 8.1227111635e3 %% Ohm
+
+R1 = R(1)*1000
+R2 = R(2)*1000
+R3 = R(3)*1000
+R4 = R(4)*1000
+R5 = R(5)*1000
+R6 = R(6)*1000
+R7 = R(7)*1000
+Vs = R(8)
+C = R(9)
+Kb = R(10)/1000
+Kd = R(11)*1000
+
 
 %% Matriz-resultado com valores numéricos das correntes nas malhas
-A = [0, 0, 0, 1; R1+R3+R4, -R3, -R4, 0; -R4, 0, R6+R7+R4-Kc, 0; -R3*Kb, R3*Kb - 1, 0, 0]
-B = [I; -V; 0; 0]
-C = A\B
+A1=[1 0 0 -1 0 0 0 0 ; 0 0 0 1 0 0 0 0 ;-1/R1 (1/R1)+(1/R2)+(1/R3) -(1/R2) 0 -(1/R3) 0 0 0 ;0 Kb+(1/R2) -(1/R2) 0 -Kb 0 0 0 ; 0 -Kb 0 0 (1/R5)+Kb -(1/R5) 0 0; 0 0 0 -(1/R6) 0 0 (1/R7)+(1/R6) -(1/R7); 0 0 0 -(1/R7)*Kd 1 0 (1/R7)*Kd -1]
+b1=[Vs 0 0 0 0 0 0]
+b1=b1'
+V1=A1\b1
 
 %% Matriz-resultado com valores numéricos da tensão nos nós do circuito (NODE)
-D = [G7, 0, 0, 0, 0, 0, G6, 0; 1, -1, 0, 0, 0, 0, 0, 0; 0, 0, 0, 0, 0, 1, -1, 0; 0, 0, 0, -G2, G1+G2+G3, -G1, 0, -G3; 0, 0, G5, 0, G3, 0, G4+G6, -G3-G4-G5; 1, 0, 0, 0, 0, 0, Kc*G6, -1; 0, 0, 0, 0, G1, -G1, -G4-G6, G4; 0, 0, 0, -G2, Kb+G2, 0, 0, -Kb]
-E = [0; 0; V; 0; I; 0; 0; 0]
-F = D\E
+A2=[1 0 0 -1 0 0 0 0 ; 0 0 0 1 0 0 0 0 ;-1/R1 (1/R1)+(1/R2)+(1/R3) -(1/R2) 0 -(1/R3) 0 0 0 ;0 Kb+(1/R2) -(1/R2) 0 -Kb 0 0 0 ; 0 0 0 0 0 1 0 -1; 0 0 0 -(1/R6) 0 0 (1/R7)+(1/R6) -(1/R7) ; 0 0 0 -(1/R7)*Kd 1 0 (1/R7)*Kd -1]
+b2=[0 0 0 0 V1(6)-V1(8) 0 0]
+b2=b2'
+V2=A2\b2
 
 %% Atribuir às variáveis os seus valores
-Ia = C(1,1)
-Ib = C(2,1)
-Ic = C(3,1)
-Id = C(4,1)
+I11=(V1(1)-V1(2))/R1
+I12=(V1(2)-V1(3))/R2
+I13=(V1(7)-V1(4))/R6
+I14=0
+I21=(V2(1)-V2(2))/R1
+I22=(V2(2)-V2(3))/R2
+I23=(V2(7)-V2(4))/R6
+I24=5
 
-V1 = F(1,1)
-V2 = F(2,1)
-V3 = F(3,1)
-V4 = F(4,1)
-V5 = F(5,1)
-V6 = F(6,1)
-V7 = F(7,1)
-V8 = F(8,1)
+f_net=fopen("circuit1.net","w");
+fprintf(f_net, "*Circuito 1\n R1 1 2 %f \n R2 2 3 %f \n R3 2 5 %f \n R4 0 5 %f \n R5 5 6 %f \n R6 0 7 %f \n R7 7 8 %f \n C 6 8 %f \n V 1 0 DC %f \n H 5 8 %f \n G 3 6 %f", R1,R2,R3,R4,R5,R6,R7,C,Vs, V1(5)-V1(8), V1(6)-V1(3));
 
+
+fclose(f_net);
+
+f_net=fopen("circuit2.net","w");
+fprintf(f_net, "*Circuito 2\n R1 0 2 %f \n R2 2 3 %f \n R3 2 5 %f \n R4 0 5 %f \n R5 5 6 %f \n R6 0 7 %f \n R7 7 8 %f \n V 6 8 %f \n H 5 8 %f \n G 3 6 %f", R1,R2,R3,R4,R5,R6,R7,V1(6)-V1(8), V1(5)-V1(8), V1(6)-V1(3));
+
+
+fclose(f_net);
+
+%{
 %% Criar 2 ficheiros, que serão o input das tabelas com os resultados teóricos
 fid = fopen("data_current_tab.tex","w")
-fprintf(fid, "@$I_{a}$ & %f \\\\ \\hline \n", Ia)
-fprintf(fid, "@$I_{b}$ & %f \\\\ \\hline \n", Ib)
-fprintf(fid, "@$I_{c}$ & %f \\\\ \\hline \n", Ic)
-fprintf(fid, "@$I_{d}$ & %f \\\\ \\hline \n", Id)
+fprintf(fid, "@$I_{a}$ & %f \\\\ \\hline \n", I11)
+fprintf(fid, "@$I_{b}$ & %f \\\\ \\hline \n", I12)
+fprintf(fid, "@$I_{c}$ & %f \\\\ \\hline \n", I13)
+fprintf(fid, "@$I_{d}$ & %f \\\\ \\hline \n", I14)
 fclose(fid)
 
 fid = fopen("data_voltage_tab.tex","w")
-fprintf(fid, "$V_{1}$ & %f \\\\ \\hline \n", V1)
-fprintf(fid, "$V_{2}$ & %f \\\\ \\hline \n", V2)
-fprintf(fid, "$V_{3}$ & %f \\\\ \\hline \n", V3)
-fprintf(fid, "$V_{4}$ & %f \\\\ \\hline \n", V4)
-fprintf(fid, "$V_{5}$ & %f \\\\ \\hline \n", V5)
-fprintf(fid, "$V_{6}$ & %f \\\\ \\hline \n", V6)
-fprintf(fid, "$V_{7}$ & %f \\\\ \\hline \n", V7)
-fprintf(fid, "$V_{8}$ & %f \\\\ \\hline \n", V8)
+fprintf(fid, "$V_{1}$ & %f \\\\ \\hline \n", V1(1))
+fprintf(fid, "$V_{2}$ & %f \\\\ \\hline \n", V1(2))
+fprintf(fid, "$V_{3}$ & %f \\\\ \\hline \n", V1(3))
+fprintf(fid, "$V_{4}$ & %f \\\\ \\hline \n", V1(4))
+fprintf(fid, "$V_{5}$ & %f \\\\ \\hline \n", V1(5))
+fprintf(fid, "$V_{6}$ & %f \\\\ \\hline \n", V1(6))
+fprintf(fid, "$V_{7}$ & %f \\\\ \\hline \n", V1(7))
+fprintf(fid, "$V_{8}$ & %f \\\\ \\hline \n", V1(8))
 fclose(fid)
+%}
