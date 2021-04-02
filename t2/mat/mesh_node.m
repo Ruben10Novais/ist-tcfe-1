339c7ab17c26eded4f6cb2ac9bac998b19b5d683
Ruben10Novais <ruben.novais2001@gmail.com>
2021-04-02 19:24:08 +0100

    #TeamGodzilla

--- t2/mat/mesh_node.m
+++ t2/mat/mesh_node.m
@@ -32,6 +32,10 @@ G5 = 1/R5
 G6 = 1/R6
 G7 = 1/R7
 
+f_net=fopen("circuit1.txt","w");
+fprintf(f_net, "*Circuito 1\nR1 1 2 %f \nR2 2 3 %f \nR3 5 2 %f \nR4 5 0 %f \nR5 6 5 %f \nR6 0 7 %f \nR7 4 8 %f \nC 6 8 %f \nV 1 0 DC %f \nVf 7 4 0 \nH 5 8 Vf %f \nG 6 3 (2,5) %f", R1,R2,R3,R4,R5,R6,R7,C,Vs, Kd, Kb);
+fclose(f_net);
+
 %% Matriz-resultado com valores numéricos das tensões nos nós
 
 A1 = [1, 0, 0, 0, 0, 0, 0 ; -G1, G1+G2+G3, -G2, -G3, 0, 0, 0; 0, Kb+G2, -G2, -Kb, 0, 0, 0 ; -G1, G1, 0, G4, 0, G6, 0 ; 0, 0, 0, 0, 0, -G6-G7, G7 ; 0, 0, 0, 1, 0, G6*Kd, -1 ; 0, -G3, 0, G3+G4+G5, -G5, G6, 0]
@@ -65,16 +69,16 @@ A2 = [1, 0, 0, 0, 0, 0, 0 ; -G1, G1+G2+G3, -G2, -G3, 0, 0, 0; 0, Kb+G2, -G2, -Kb
 b2 = [0; 0; 0; 0; 0; 0; Vx]
 V2 = A2\b2
 
-V_1 = V2(1)
-V_2 = V2(2)
-V_3 = V2(3)
-V_4 = 0
-V_5 = V2(4)
-V_6 = V2(5)
-V_7 = V2(6)
-V_8 = V2(7)
+V_11 = V2(1)
+V_22 = V2(2)
+V_33 = V2(3)
+V_44 = 0
+V_55 = V2(4)
+V_66 = V2(5)
+V_77 = V2(6)
+V_88 = V2(7)
 
-Ix = ((V_6-V_5)/R5) + ((V_3-V_2)/R2)
+Ix = ((V_66-V_55)/R5) + ((V_33-V_22)/R2)
 
 Req = abs(Vx/Ix)
 
@@ -91,7 +95,7 @@ V6n(t) = A*exp(wn*t)
 A = Vx
 wn = - (1/tau)
 
-t=0:1e-6:20e-3;
+t=0:1e-5:20e-3;
 V6n = A*exp(wn*t)
 
 hf = figure (1);
@@ -99,7 +103,54 @@ plot (t*1000, V6n, "g");
 
 xlabel ("t[ms]");
 ylabel ("v6_n [V]");
-print (hf, "natural.eps", "-natura");
+print (hf, "natural.eps", "-depsc");
+
+%% Forced solution
+w = 2*pi*1000
+
+A3 = [1, 0, 0, 0, 0, 0, 0 ; -G1, G1+G2+G3, -G2, -G3, 0, 0, 0; 0, Kb+G2, -G2, -Kb, 0, 0, 0 ; -G1, G1, 0, G4, 0, G6, 0 ; 0, 0, 0, 0, 0, -G6-G7, G7 ; 0, 0, 0, 1, 0, G6*Kd, -1 ; 0, -G3, 0, G3+G4+G5, -G5-(j*w*C), G6, j*w*C]
+b3 = [-j; 0; 0; 0; 0; 0; 0]
+V3 = A3\b3
+
+V1r = abs(V3(1))
+V1teta = angle(V3(1))
+V2r = abs(V3(2))
+V2teta = angle(V3(2))
+V3r = abs(V3(3))
+V3teta = angle(V3(3))
+V4r = 0
+V4teta = 0
+V5r = abs(V3(4))
+V5teta = angle(V3(4))
+V6r = abs(V3(5))
+V6teta = angle(V3(5))
+V7r = abs(V3(6))
+V7teta = angle(V3(6))
+V8r = abs(V3(7))
+V8teta = angle(V3(7))
+
+%% Total solution
+V6f = V6r*cos((w*t)+V6teta)
+V6t = V6n + V6f
+
+tneg=-5e-3:1e-5:0;
+tt=[tneg,t];
+Vsneg = Vs + 0*tneg
+Vspos = sin(w*t)
+V6neg = V_6 + 0*tneg
+V6pos = V6t
+Vs_t = [Vsneg,Vspos];
+V6_t = [V6neg,V6pos];
+
+hf2 = figure (2);
+plot (tt, Vs_t, "b");
+hold on
+plot (tt, V6_t, "r");
+
+xlabel ("t[ms]");
+ylabel ("V_s(t) , V_6(t) [V]");
+legend ('V_s(t)','V_6(t)','Location','Northeast')
+print (hf2, "total.eps", "-depsc");
 
 %%
 %f_net=fopen("circuit1.net","w");
