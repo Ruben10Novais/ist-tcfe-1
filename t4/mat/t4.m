5abd2c597a46e2ed40c4e0f4ec7625b4fc376931
Ruben10Novais <ruben.novais2001@gmail.com>
2021-05-21 18:41:12 +0100

    Falta frequency response

--- t4/mat/t4.m
+++ t4/mat/t4.m
@@ -3,8 +3,8 @@
 VT=25e-3
 BFN=178.7
 VAFN=69.7
-RE1=100
-RC1=1000
+RE1=200
+RC1=950
 RB1=80000
 RB2=20000
 VBEON=0.7
@@ -23,6 +23,8 @@ fprintf(fid, "VBEON& %f \\\\ \\hline \n", VBEON)
 fprintf(fid, "VCC& %f \\\\ \\hline \n", VCC)
 fprintf(fid, "RS& %f \\\\ \\hline \n", RS)
 fclose(fid)
+
+%Operating point
 
 RB=1/(1/RB1+1/RB2)
 VEQ=RB2/(RB1+RB2)*VCC
@@ -44,48 +46,55 @@ fprintf(fid, "VO1%f \\\\ \\hline \n", VO1)
 fprintf(fid, "VCE%f \\\\ \\hline \n", VCE)
 fclose(fid)
 
+%Incremental
+
 gm1=IC1/VT
 rpi1=BFN/gm1
 ro1=VAFN/IC1
 
-AV1 = RC1*(RE1-gm1*rpi1*ro1)/((ro1+RC1+RE1)*(RB+rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)
-
-AV1simple = gm1*RC1/(1+gm1*RE1)
-
-RE1=0
-AV1 = RC1*(RE1-gm1*rpi1*ro1)/((ro1+RC1+RE1)*(RB+100+rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)
-AV1simple = gm1*RC1/(1+gm1*RE1)
-
-RE1=100
-
-ZI1 = ((ro1+RC1+RE1)*(RB+rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)/(ro1+RC1+RE1)
-
-ZX = ro1*((RB+rpi1)*RE1/(RB+rpi1+RE1))/(1/(1/ro1+1/(rpi1+RB)+1/RE1+gm1*rpi1/(rpi1+RB)))
-
-ZO1 = 1/(1/ZX+1/RC1)
-
+ZI1 = 1/((1/RB)+(1/rpi1))
+ZO1 = 1/((1/RC1)+(1/ro1))
+AV1 = - gm1*ZO1*(ZI1/(ZI1+RS))
+AV1dB = 20*log10(abs(AV1))
 
 
 %output stage
+
+%Operating point
+
 BFP = 227.3
 VAFP = 37.2
-RE2 = 100
+RE2 = 50
 VEBON = 0.7
 VI2 = VO1
 IE2 = (VCC-VEBON-VI2)/RE2
 IC2 = BFP/(BFP+1)*IE2
 VO2 = VCC - RE2*IE2
 
+%Incremental
 
 gm2 = IC2/VT
 go2 = IC2/VAFP
 gpi2 = gm2/BFP
 ge2 = 1/RE2
 
-AV2 = gm2/(gm2+gpi2+go2+ge2)
-
-
-
-ZI2 = (gm2+gpi2+go2+ge2)/gpi2/(gpi2+go2+ge2)
-
+AV2 = (gm2+gpi2)/(gm2+gpi2+go2+ge2)
+AV2dB = 20*log10(AV2)
+ZI2=(1/gpi2)/(1-AV2)
 ZO2 = 1/(gm2+gpi2+go2+ge2)
+
+
+%Circuito geral
+
+rpi2=1/gpi2
+
+ZI=ZI1
+ZO = 1/(go2+(gm2*(rpi2/(rpi2+ZO1)))+ge2+(1/(rpi2+ZO1)))
+
+prep1=1/(rpi2+ZO1)
+prep2=(gm2*rpi2)/(rpi2+ZO1)
+AV=(prep1+prep2)/(prep1+ge2+go2+prep2)*AV1
+AVdB=20*log10(abs(AV))
+
+%frequency response
+
