e8d723f2004f8b9ee10c8ec7b0881b3b396f6f2e
DiogoFaustino1 <diogovicentefaustino@tecnico.ulisboa.pt>
2021-06-07 22:34:44 +0100

    vou tratar da conclusão agora

--- t5/mat/t5.m
+++ t5/mat/t5.m
@@ -36,14 +36,22 @@ print(fig2, "gain.eps", "-depsc");
 
 wL = 1/(R1*C1)
 wH = 1/(R2*C2)
-wO = sqrt(wL*wH)
+wO1 = sqrt(wL*wH)
+AV_HP = abs((R1*C1*j*wO1)/(1+R1*C1*j*wO1))
+AV_HPdb = 20*log10(AV_HP)
+AV_Amp = (1+R4/R3)
+AV_Ampdb = 20*log10(AV_Amp)
+AV_LP = abs(1/(1+R2*C2*j*wO1))
+AV_LPdb = 20*log10(AV_LP)
+AV = AV_HP*AV_Amp*AV_LP
+AVdb1 = AV_HPdb + AV_Ampdb + AV_LPdb
 
 fid = fopen("frequency_tab.tex","w")
 fprintf(fid, "Lower Cut-Off Frequency & %f Hz \\\\ \\hline \n", wL/(2*pi))
 fprintf(fid, "Upper Cut-Off Frequency & %f Hz \\\\ \\hline \n", wH/(2*pi))
-fprintf(fid, "Central Frequency & %f Hz \\\\ \\hline \n", wO/(2*pi))
+fprintf(fid, "Central Frequency & %f Hz \\\\ \\hline \n", wO1/(2*pi))
 fclose(fid)
-
+wO=2*pi*1000
 AV_HP = abs((R1*C1*j*wO)/(1+R1*C1*j*wO))
 AV_HPdb = 20*log10(AV_HP)
 AV_Amp = (1+R4/R3)
@@ -51,16 +59,17 @@ AV_Ampdb = 20*log10(AV_Amp)
 AV_LP = abs(1/(1+R2*C2*j*wO))
 AV_LPdb = 20*log10(AV_LP)
 AV = AV_HP*AV_Amp*AV_LP
-AVdb = AV_HPdb + AV_Ampdb + AV_LPdb
+AVdb2 = AV_HPdb + AV_Ampdb + AV_LPdb
 z_in = R1 + 1/(j*wO*C1)
 z_out = R2/(1+j*wO*R2*C2)
 
 cost = 13323.29204 + (R1+R2+R3+(R4-50000)*3)*0.001 + (C1+(C2*2)*2)*1000000
-merit = 1/(cost*(abs(AV-100)+abs((wO/(2*pi))-1000)+(1e-6)))
+merit = 1/(cost*(abs(AV-100)+abs((wO1/(2*pi))-1000)+(1e-6)))
 
 fid = fopen("theo_tab.tex","w")
-fprintf(fid, "Gain & %f \\\\ \\hline \n", AV)
-fprintf(fid, "Gain (dB) & %f dB \\\\ \\hline \n", AVdb)
+fprintf(fid, "Gain (1 KHz) & %f \\\\ \\hline \n", AV)
+fprintf(fid, "Gain (1 KHz)(dB) & %f dB \\\\ \\hline \n", AVdb2)
+fprintf(fid, "Gain (calculated central frequency) (dB) & %f dB \\\\ \\hline \n", AVdb1)
 fprintf(fid, "$Z_{in}$ & %f %fj Ohm \\\\ \\hline \n", real(z_in), imag(z_in))
 fprintf(fid, "$Z_{in}$ modulus & %f Ohm \\\\ \\hline \n", abs(z_in))
 fprintf(fid, "$Z_{in}$ phase & %f Degrees \\\\ \\hline \n", arg(z_in)*180/pi)
@@ -70,3 +79,24 @@ fprintf(fid, "$Z_{out}$ phase & %f Degrees \\\\ \\hline \n", arg(z_out)*180/pi)
 fprintf(fid, "Cost & %fMU \\\\ \\hline \n", cost)
 fprintf(fid, "Merit & %f$*10^{-6}$ \\\\ \\hline \n", merit*1000000)
 fclose(fid)
+
+
+wO=4545.5
+AV_HP = abs((R1*C1*j*wO)/(1+R1*C1*j*wO))
+AV_HPdb = 20*log10(AV_HP)
+AV_Amp = (1+R4/R3)
+AV_Ampdb = 20*log10(AV_Amp)
+AV_LP = abs(1/(1+R2*C2*j*wO))
+AV_LPdb = 20*log10(AV_LP)
+AV = AV_HP*AV_Amp*AV_LP
+AVdb2 = AV_HPdb + AV_Ampdb + AV_LPdb
+
+wO= 9090.9
+AV_HP = abs((R1*C1*j*wO)/(1+R1*C1*j*wO))
+AV_HPdb = 20*log10(AV_HP)
+AV_Amp = (1+R4/R3)
+AV_Ampdb = 20*log10(AV_Amp)
+AV_LP = abs(1/(1+R2*C2*j*wO))
+AV_LPdb = 20*log10(AV_LP)
+AV = AV_HP*AV_Amp*AV_LP
+AVdb2 = AV_HPdb + AV_Ampdb + AV_LPdb
